ce7fea443d5d305b437f373219996a19df8b5944
michael2tavares <srmichael45@gmail.com>
2021-05-17 12:28:04 -0300

    evento de click
    Descrevi uma função de evento de click do mouse.

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,5 @@
 const meuCabecalho = document.querySelector ('h1');
 meuCabecalho.textContent = 'Ola Mundo!';
-function multiplica (num1, num2){
-    let resultado = num1*num2;
-    return resultado;
+document.querySelector ('html').onclick = function(){
+alert ('Ai! Pare de me cutucar!');
 }
