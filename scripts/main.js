6aee186c3a0d3966ffc584d01deb27ed1eb756d1
michael2tavares <srmichael45@gmail.com>
2021-05-17 12:15:06 -0300

    Testando Minha Primeira Função
    Função que multiplica variavel num1 com variavel num2.

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,6 @@
 const meuCabecalho = document.querySelector ('h1');
 meuCabecalho.textContent = 'Ola Mundo!';
+function multiplica (num1, num2){
+    let resultado = num1*num2;
+    return resultado;
+}
